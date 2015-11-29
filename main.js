10836f9985ece1dc815d1ab53eb58f4ffcbfed46
bobziemerman <bobziemerman@gmail.com>
2015-11-29 11:49:24 -0500

    Tweaks on mobile and (mostly) desktop

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
+    resizeNav();
     resizeCardImages();
 });
 
 $(window).resize(function(){
+    resizeNav();
     resizeCardImages();
 });
 
-//TODO do this on page resize too
+
+//Makes the nav elements column in the nav the height of the brand column
+var resizeNav = function(){
+console.log("nav resized from "+$('#js-nav-elements').height()+" to "+$('nav').height());
+    $('#js-nav-elements').height($('nav').height());
+console.log($('#js-nav-elements').height());
+};
+
 //Set the card image heights to dynamically match the widths
 var resizeCardImages = function(){
-    $('.js-resize').css({'height':$('.js-resize').width()+'px'});
+    $('.js-resize').height($('.js-resize').width());
 };
