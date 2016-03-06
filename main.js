5e1ae36811c41f5677ed2965a4d5717eef3d3eba
bobziemerman <bobziemerman@gmail.com>
2016-03-06 15:18:33 -0500

    Did some more work, need to move to server to start PHP

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
-    
+    $("#sort--price").click(function(){
+console.log("price");
+        
+        //$(".flex-card").get().sort(sortByName);
+    });
 });
 
 $(window).resize(function(){
     //Close any open modals if the screen is resized to Large
     if($(document).width() > 992){
-        $(".modal").closeModal();
+        $(".modal__card").closeModal();
     }
 });
 
@@ -15,3 +19,14 @@ function conditionalCardModal(modalId){
     if($(document).width() <= 992)
         $(modalId).openModal();
 }
+
+
+/********************************
+            SORTERS
+*********************************/
+
+/* Don't need; sorting will be done server-side
+var sortByPrice = function(a,b){
+    return a.find(".card--price").text().localeCompare(b.find(".card--price").text());
+}
+*/
