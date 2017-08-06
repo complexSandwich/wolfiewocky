afc46919020368e922bccb2feb91abaa7c6085df
complexSandwich <briangzimmerman@gmail.com>
2017-08-06 12:48:06 -0500

    added price and name sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,4 +27,21 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     this.getCards();
   }
+
+  nameSort(): void {
+    this.filtered.sort(function(a,b){
+        if(a.name < b.name)
+            return -1;
+        else if(a.name > b.name)
+            return 1;
+        else
+            return 0;
+    });
+  }
+
+  priceSort(): void {
+    this.filtered.sort(function(a,b) {
+        return a.price - b.price;
+    });
+  }
 }
