991c78d56543aa02e970ebc385a80995a0c7831c
Brian Zimmerman <briangzimmerman@gmail.com>
2017-08-30 18:32:29 -0500

    Now modal windows open

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,31 +1,14 @@
-$(document).ready(function(){
-    $("#sort--price").click(function(){
-       //TODO: remove these function 
-        //$(".flex-card").get().sort(sortByName);
-    });
-});
-
 $(window).resize(function(){
     //Close any open modals if the screen is resized to Large
     if($(document).width() > 992){
-        $(".modal__card").closeModal();
+        $(".modal").closeModal();
+        $(".lean-overlay").hide(); //Above line closes modal, background was still greyed out.
     }
 });
 
-
 //Only open card modals in Medium or Small modes
-function conditionalCardModal(modalId){
-    if($(document).width() <= 992)
-        $(modalId).openModal();
+function conditionalCardModal(identifier){
+    console.log($(identifier).data('test'));
+    if($(document).width() <= 992)    
+        $('#' + $(identifier).data('target')).openModal();
 }
-
-
-/********************************
-            SORTERS
-*********************************/
-
-/* Don't need; sorting will be done server-side
-var sortByPrice = function(a,b){
-    return a.find(".card--price").text().localeCompare(b.find(".card--price").text());
-}
-*/
