5fb32eb82629df892669219a78b321660c6221ac
Brian Zimmerman <briangzimmerman@gmail.com>
2018-01-26 23:26:26 -0600

    updating

---
+++ public/js/test-data.js
@@ -0,0 +1,12 @@
+var cards = [
+    { id: 0, name: 'One', price: 100, description: 'bla blah balg', category: 'ALICE', dateAdded: new Date(), image: 'cheshire.jpg' },
+    { id: 1, name: 'Two', price: 2, description: 'bla blah balg', category: 'MARVEL', dateAdded: new Date(), image: 'cheshire.jpg' },
+    { id: 2, name: 'Three', price: 300, description: 'bla blah balg', category: 'DC', dateAdded: new Date(), image: 'cheshire.jpg' },
+    { id: 3, name: 'Four', price: 40, description: 'bla blah balg', category: 'ARCHEY', dateAdded: new Date(), image: 'cheshire.jpg' },
+    { id: 4, name: 'Five', price: 500, description: 'bla blah balg', category: 'DISNEY', dateAdded: new Date(), image: 'cheshire.jpg' },
+    { id: 5, name: 'Six', price: 60, description: 'bla blah balg', category: 'ICON', dateAdded: new Date(), image: 'cheshire.jpg' },
+    { id: 6, name: 'Seven', price: 700.96, description: 'bla blah balg', category: 'ALICE', dateAdded: new Date(), image: 'cheshire.jpg' },
+    { id: 7, name: 'Eight', price: 800, description: 'bla blah balg', category: 'MARVEL', dateAdded: new Date(), image: 'cheshire.jpg' },
+    { id: 8, name: 'Nine', price: 900, description: 'bla blah balg', category: 'DC', dateAdded: new Date(), image: 'cheshire.jpg' },
+    { id: 9, name: 'Ten', price: 12, description: 'bla blah balg', category: 'ARCHEY', dateAdded: new Date(), image: 'cheshire.jpg' }
+];
